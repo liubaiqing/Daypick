e7fa799990e48e4e65197c564234bdf58965704e
liubaiqing <2484264106@qq.com>
2026-08-29 16:49:48 +0800

    chore: 更新插件注册生成文件（winrt_ocr_flutter）

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,18 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <pasteboard/pasteboard_plugin.h>
+#include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
+#include <window_manager/window_manager_plugin.h>
+#include <winrt_ocr_flutter/winrt_ocr_flutter_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PasteboardPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PasteboardPlugin"));
+  ScreenRetrieverWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverWindowsPluginCApi"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
+  WinrtOcrFlutterPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WinrtOcrFlutterPluginCApi"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,14 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  pasteboard
+  screen_retriever_windows
+  window_manager
+  winrt_ocr_flutter
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
